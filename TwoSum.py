8d4a31c5ca61cf8341f62df31a4e11005b86039e
Nisha Gurunath Bharathi <nishagb@mail.fresnostate.edu>
2018-01-27 00:42:13 -0800

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -1,3 +1,11 @@
+#Author: Nisha Gurunath Bharathi
+#Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+#You may assume that each input would have exactly one solution, and you may not use the same element twice.
+#Example:
+#Given nums = [2, 7, 11, 15], target = 9,
+#Because nums[0] + nums[1] = 2 + 7 = 9,
+#return [0, 1].
+
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -6,8 +14,8 @@ class Solution(object):
         :rtype: List[int]
         """
         sum=0;
-        for i in range(0,len(nums)):   #for(i=0;i<len(nums);i++)
-                for j in range(i+1,len(nums)):     #for(j=i+1;j<len(nums);j++):
+        for i in range(0,len(nums)):   #for(i=0;i<len(nums);i++) //java for loop
+                for j in range(i+1,len(nums)):     #for(j=i+1;j<len(nums);j++): //java for loop
                     sum=0
                     sum=nums[i]+nums[j]
                     if (sum == target): 
